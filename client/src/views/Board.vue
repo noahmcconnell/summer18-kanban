e5490d6ca8ab57a561a6a527cd70a9c8cb069962
Chace Hartwell <chace.l.hartwell@icloud.com>
2018-09-10 11:28:53 -0600

    More Style

--- client/src/views/Board.vue
+++ client/src/views/Board.vue
@@ -4,12 +4,13 @@
       <button @click="logout">Logout</button>
     </div>
     <h1 style="color: white; text-shadow: 3px 3px black;">Create a List</h1>
-    <form @submit.prevent="addList">
+    <form style="margin-bottom: 20px;" @submit.prevent="addList">
       <input type="text" placeholder="List Name" v-model="newList.name" required>
       <input type="text" placeholder="Description" v-model="newList.description">
       <button type="submit">Create List</button>
     </form>
-    <div class="row" style="margin-top: 5rem">
+    <h4 style="color: white; text-shadow: 2px 2px black; margin: 40px;">LISTS:</h4>
+    <div class="row">
     <div class="col-sm-4" v-for="list in lists" :key="list._id" >
       <!-- list component here -->
       <list :listData='list' />
